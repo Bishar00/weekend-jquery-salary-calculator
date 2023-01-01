1c133fc932747c9b28264c718bfad32365c9caa0
Bishar00 <bishar1yusuf@gmail.com>
2023-01-01 11:31:11 -0600

    Fixed issue that was creating a bug.

--- script.js
+++ script.js
@@ -1,110 +1,72 @@
+let monthlySalary = 0;
+
 // This is where I get the Dom
 $(document).ready(onReady);
 //
-function onReady(){
-    console.log('DOM is loaded');
-    //add click event handler that calls the AddEmployee
-    // ButtonToAddEmployees is on the index as a button and when that button is clicked we use the submitaddEmployees fuction below
-    $('#buttonToAddEmployees').on('click',submitaddEmployees)
+function onReady() {
+  console.log("DOM is loaded");
 
-    $('body').on('click',"#removeAddEmployees",deletebutton)
+  $("#buttonToAddEmployees").on("click", submitaddEmployees);
 
-    $('#buttonToAddEmployees').on('click',calculateSalary)
+  $("body").on("click", "#removeAddEmployees", deletebutton);
 
-    
-
+  $("#buttonToAddEmployees").on("click", calculateSalary);
 }
 
+function clearValFunc() {
+  $("#theFirstName").val("");
+  $("#theLastName").val("");
+  $("#EmployeeID").val("");
+  $("#theTitle").val("");
+  $("#theAnnualSalary").val("");
+}
 
-//This is an array
-let employees = [];
-//This is where I created our monthly variable so that I can make it go red
-let monthlySalary = 0;
+function submitaddEmployees() {
+  //This is an object and we will use it
+  let employeeObject = {
+    firstName: $("#theFirstName").val(),
+    lastName: $("#theLastName").val(),
+    id: $("#EmployeeID").val(),
+    title: $("#theTitle").val(),
+    salary: $("#theAnnualSalary").val(),
+  };
+  calculateSalary(employeeObject.salary);
+  clearValFunc();
+  displaySalary(employeeObject);
+}
 
-// We make variable to connect with the inputs on the index.html and we use .val()
-function submitaddEmployees(){
-    //get input values
-    //set varaible to index.html input IDs and .val()is facilitating the connection               
-    let FirstName =$('#theFirstName').val()
-    let LastName =$('#theLastName').val()
-    let ID=$('#EmployeeID').val()
-    let Title=$('#theTitle').val()
-    let AnnualSalary=$('#theAnnualSalary').val()
-
-//This is an object and we will use it
-    let addNewEmployeeObject= {
-    First: FirstName,
-    Last: LastName,
-    ID: Number(ID),
-    Title:Title,
-    Salary:Number(AnnualSalary)
-
-    }
-
-    //Employees is our Array and .push() is pushing the object into our Empty Array
-    employees.push(addNewEmployeeObject)
-
-    //Below is a for loop which run through the entire object
-    for(let i=0; i<employees.length; i++){
-
-    //Below is the table from the index.html
-    //We are using append to insert specified content at the end of the selected element which is the Table in this case
-    $('table').append(`
-    <TR id='Employees'>
-            <td>${employees[i].First}</td>
-            <td>${employees[i].Last}</td>
-            <td>${employees[i].ID}</td>
-            <td>${employees[i].Title}</td>
-            <td>${employees[i].Salary}</td>
+function displaySalary(employeeSal) {
+  $("table").append(`
+    <tr id='Employees'>
+            <td>${employeeSal.firstName}</td>
+            <td>${employeeSal.lastName}</td>
+            <td>${employeeSal.id}</td>
+            <td>${employeeSal.title}</td>
+            <td>${employeeSal.salary}</td>
             <td> <button id="removeAddEmployees">Delete</button> </td>
-
-        </TR>
-    `)
+        </tr>
+    `);
 }
 
-
-
+//This is where we created a fuction for the delete
+function deletebutton() {
+  $(this).parent().parent().remove();
 }
 
+function calculateSalary(yearlySalary) {
+  yearlySalary = $("#theAnnualSalary").val();
+  console.log(`MonthLy Sal ${yearlySalary}`);
+  console.log(yearlySalary, "yearly salary");
+  monthlySalary += yearlySalary / 12;
 
-//This is where we created a fuction for the delete 
-function deletebutton(){
-$(this).parent().parent().remove()
+  $("#thingsinthespan").text(monthlySalary);
+
+  render(monthlySalary);
 }
-function calculateSalary(){
-
-//    let yearlySalary= $('#theAnnualSalary').val()
-//    console.log('yearlySal', yearlySalary)
-//     let Salary = yearlySalary / 12;
-//     // monthlySalary= Salary + monthlySalary
-//     $('#thingsinthespan').text(monthlySalary)
-//     render (monthlySalary)
-
-let yearlySalary = $('#theAnnualSalary').val()
-console.log(yearlySalary, 'yearly salary');
- monthlySalary += yearlySalary / 12;
-
- $('#thingsinthespan').text(monthlySalary);
-
-
-render(monthlySalary)
-
-
+function render(monthlySal) {
+  if (monthlySal >= 20000) {
+    return $("h4").css("background-color", "red");
+  } else {
+    return $("h4").css("background-color", "green");
+  }
 }
-function render( monthlySal){
-
-    if(monthlySal >=  20000)  return   $('h4').css('background-color','red')
-    
-    $('#theFirstName').val('')
-    $('#theLastName').val('')
-    $('#EmployeeID').val('')
-    $('#theTitle').val('')
-    $('#theAnnualSalary').val('')
-
-    // if(monthlySalary > 20000){
-    //     $('h4').css('background-color','green')
-    // }
-
-}
-
-
